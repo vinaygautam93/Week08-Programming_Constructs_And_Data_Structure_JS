b34663b0aca2d92085c1fd097fc8bfce26333f8e
vinaygautam93 <vinaygautam935680@gmail.com>
2025-03-05 10:22:44 +0530

    [Vinay] Added NUMberIntoWord.js in CaseStatement

---
+++ Day01/CaseStatement/NUmberIntoWord.js
@@ -0,0 +1,37 @@
+// Generate a random number between 1 and 9
+let weekDay=Math.floor(Math.random() * 10) ;
+
+console.log("Week day is "+weekDay);
+
+switch(weekDay){    
+    case 0:
+        console.log(arr[0]);    
+        break;
+    case 1:
+        console.log(arr[1]);    
+        break;
+    case 2:
+        console.log(arr[2]);    
+        break;
+    case 3:
+        console.log(arr[3]);    
+        break;
+    case 4:
+        console.log(arr[4]);    
+        break;
+    case 5:
+        console.log(arr[5]);    
+        break;
+    case 6:
+        console.log(arr[6]);    
+        break;
+    case 7:
+        console.log(arr[7]);    
+        break;    
+    case 8:
+        console.log(arr[8]);    
+        break;    
+    case 9:
+        console.log(arr[9]);    
+        break;    
+}
